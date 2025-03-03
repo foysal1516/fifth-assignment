e0122ee4cb9755ba8bfe0ad9ab8d422b1b992cb9
unknown <foysal1516@gmail.com>
2025-03-03 17:24:42 +0600

    redirect to blog page

--- js/features.js
+++ js/features.js
@@ -5,3 +5,7 @@ document.getElementById('btn-history').addEventListener('click', function () {
 document.getElementById('btn-donation').addEventListener('click', function () {
     showASectionById('cart-container')
 })
+// redirect the blog page
+document.getElementById('blog').addEventListener('click', function () {
+    window.location.href = 'blog.html';
+})
